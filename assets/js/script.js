7da87d38ec1448ab6b18661425b5635f04ed611f
Mengxiao Qi <mengxiaoqi@MacBook-Air.local>
2021-09-08 21:24:52 -0500

    fix api link to allinone

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,57 @@
 var detailWeather = document.getElementById("detailWeather");
 var cityInput = document.getElementById("cityInput");
 var weatherUrlCurrent;
+var weatherUrlAll;
 var city = document.getElementById("city");
 var date = document.getElementById("date");
 var temp = document.getElementById("temp");
 var humidity = document.getElementById("humidity");
 var wind = document.getElementById("wind");
 var icon = document.getElementById("icon");
+var uvi = document.getElementById("uvi");
+var lat;
+var lon;
 
 function searchCity() {
      document.addEventListener("click", function(event) {
           var element = event.target;
           if (element.matches("#searchBtn")) {
                weatherUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=0bc67a3f3f64547ee2e870ae76499e59`
-               showWeather();
+               getCoord();
           }
      })
 }
 searchCity();
 
-
-function showWeather() {
+function getCoord() {
      fetch(weatherUrlCurrent)
+     .then(function(response) {
+          return response.json();
+     })
+     .then(function(data) {
+          lat = data.coord.lat;
+          lon = data.coord.lon;
+          console.log(lat);
+          console.log(lon);
+          weatherUrlAll = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,daily&appid=0bc67a3f3f64547ee2e870ae76499e59`
+          getWeather();
+     })     
+}
+
+function getWeather() {
+     fetch(weatherUrlAll)
      .then(function(response) {
           return response.json();
      })
      .then(function(data) {
           city.textContent = `${cityInput.value.toUpperCase()} Weather`;
           date.textContent = moment().format("MMM, D, YYYY");
-          var iconNo = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+          var iconNo = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`;
           console.log(iconNo);
           icon.setAttribute("src", iconNo);
-          temp.textContent = `Temperature: ${data.main.temp}`; 
-          humidity.textContent = `Humidity: ${data.main.humidity}`;
-          wind.textContent = `Wind Speed: ${data.wind.speed}`;
+          temp.textContent = `Temperature: ${data.current.temp}`; 
+          humidity.textContent = `Humidity: ${data.current.humidity}`;
+          wind.textContent = `Wind Speed: ${data.current.wind_speed}`;
+          uvi.textContent = `Uvi: ${data.current.uvi}`;
      })
 }
